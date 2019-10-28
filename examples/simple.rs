683bdbc8eb3110c036f1a7d35d2e10ac26b837d0
Markus Røyset <maroider@protonmail.com>
2019-10-28 23:18:47 +0100

    Get basic communication going

---
+++ examples/simple.rs
@@ -0,0 +1,137 @@
+use std::io::{self, Write};
+
+use serialport::{SerialPortInfo, SerialPortType};
+
+fn main() {
+    match serialport::available_ports() {
+        Ok(ports) => match ports.len() {
+            0 => println!("Could not find any serial ports"),
+            1 => {
+                print_serial_port_info(&ports[0], None);
+                talk_to_tilt_thing(&ports[0]);
+            }
+            _ => {
+                for (i, port) in ports.iter().enumerate() {
+                    print_serial_port_info(port, Some(&i.to_string()));
+                }
+                print!("Select a COM port: ");
+                io::stdout().lock().flush().unwrap();
+                let selected_port = {
+                    let mut buffer = String::new();
+                    io::stdin().read_line(&mut buffer).unwrap();
+                    usize::from_str_radix(buffer.trim(), 10)
+                        .unwrap_or_else(|_| panic!("Invalid number!"))
+                };
+                let port = ports
+                    .get(selected_port)
+                    .unwrap_or_else(|| panic!("Invalid option!"));
+                talk_to_tilt_thing(&port);
+            }
+        },
+        Err(err) => {
+            println!(
+                "An error uccured while searching for available serial ports: {}",
+                err
+            );
+        }
+    }
+}
+
+fn print_serial_port_info(port_info: &SerialPortInfo, prefix: Option<&str>) {
+    let indent_base = if prefix.is_some() { 2 } else { 0 };
+
+    if let Some(prefix) = prefix {
+        println!("{}:", prefix);
+    }
+
+    println!("{}{}", " ".repeat(indent_base), port_info.port_name);
+
+    match &port_info.port_type {
+        SerialPortType::UsbPort(usb_port_info) => {
+            println!("{}Type: USB", " ".repeat(indent_base * 2));
+
+            println!("{}VID: {}", " ".repeat(indent_base * 3), usb_port_info.vid);
+            println!("{}PID: {}", " ".repeat(indent_base * 3), usb_port_info.pid);
+            if let Some(serial_number) = &usb_port_info.serial_number {
+                println!(
+                    "{}Serial Number: {}",
+                    " ".repeat(indent_base * 3),
+                    serial_number
+                );
+            }
+            if let Some(manufacturer) = &usb_port_info.manufacturer {
+                println!(
+                    "{} Manufacturer: {}",
+                    " ".repeat(indent_base * 3),
+                    manufacturer
+                );
+            }
+            if let Some(product) = &usb_port_info.product {
+                println!("{}      Product: {}", " ".repeat(indent_base * 3), product);
+            }
+        }
+        SerialPortType::PciPort => {
+            println!("{}Type: PCI", " ".repeat(indent_base * 2));
+        }
+        SerialPortType::BluetoothPort => {
+            println!("{}Type: Bluetooth", " ".repeat(indent_base * 2));
+        }
+        SerialPortType::Unknown => {
+            println!("{}Type: Unknown", " ".repeat(indent_base * 2));
+        }
+    }
+}
+
+fn talk_to_tilt_thing(port: &SerialPortInfo) {
+    let mut port = serialport::open(&port.port_name).unwrap_or_else(|err| {
+        panic!(
+            "Could not open serial port {}: {}",
+            port.port_name, err.description
+        )
+    });
+
+    println!("Sending connection request");
+    port.write(&[SerialMessageType::ConnectionRequest as u8])
+        .unwrap();
+    let mut buffer = [0u8; 2];
+    while port.bytes_to_read().unwrap() == 0 {
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+    port.read(&mut buffer).unwrap();
+    if buffer[0] != SerialMessageType::ConnectionAccepted as u8 {
+        panic!("Connection request was not accepted");
+    }
+    port.write(&[SerialMessageType::ConnectionAcknowledged as u8])
+        .unwrap();
+
+    println!("Connectino established");
+
+    loop {
+        while port.bytes_to_read().unwrap() < 2 {
+            std::thread::sleep(std::time::Duration::from_millis(100));
+        }
+
+        port.read(&mut buffer).unwrap();
+        if buffer[0] != SerialMessageType::Tilt as u8 {
+            panic!("Only tilt messages are allowed once connected");
+        }
+        let tilted = match buffer[1] {
+            0 => false,
+            1 => true,
+            n => panic!("Invalid bool: {}", n),
+        };
+
+        if tilted {
+            println!("tilted");
+        } else {
+            println!("untilted");
+        }
+    }
+}
+
+enum SerialMessageType {
+    ConnectionRequest = 0x1,
+    ConnectionAccepted = 0x2,
+    ConnectionAcknowledged = 0x3,
+    Tilt = 0x4,
+}
--- tilt_thing.ino
+++ tilt_thing.ino
@@ -6,12 +6,13 @@ const long POLL_INTERVAL = 100;
 // Serial communication constants
 const byte SC_CONN_REQ = 0x1;
 const byte SC_CONN_ACC = 0x2;
-const byte SC_TILT = 0x3;
+const byte SC_CONN_ACK = 0x3;
+const byte SC_TILT = 0x4;
 
 // BEGIN GLOBAL STATE
 
 unsigned long previous_time = 0;
-int switch_state = 0;
+byte prev_switch_state = 0;
 
 // END GLOBAL STATE
 
@@ -24,15 +25,16 @@ void setup()
     Serial.begin(9600);
     while (!Serial || Serial.available() != 1)
     {
-        ;
+        async_blink(500);
     }
     if (Serial.read() == SC_CONN_REQ)
     {
+        Serial.write(SC_CONN_ACC);
         while (Serial.available() != 1)
         {
-            ;
+            async_blink(250);
         }
-        if (Serial.read() == SC_CONN_ACC)
+        if (Serial.read() == SC_CONN_ACK)
         {
             return;
         }
@@ -47,10 +49,13 @@ void loop()
     {
         previous_time = current_time;
 
-        switch_state = digitalRead(SWITCH_PIN);
+        byte switch_state = digitalRead(SWITCH_PIN);
+        if (switch_state != prev_switch_state) {
+            prev_switch_state = switch_state;
+            Serial.write(SC_TILT);
+            Serial.write(switch_state);
+        }
     }
-
-    digitalWrite(LED_PIN, switch_state);
 }
 
 void err_blink()
@@ -59,7 +64,23 @@ void err_blink()
     while (true)
     {
         digitalWrite(LED_BUILTIN, on);
-        delay(500);
+        delay(750);
         on = !on;
+    }
+}
+
+// BEGIN GLOBAL STATE
+
+unsigned long previous_blink = 0;
+bool led_state = false;
+
+// END GLOBAL STATE
+
+void async_blink(unsigned long duration) {
+    unsigned long current_time = millis();
+    if (current_time - previous_blink > duration)  {
+        previous_blink = current_time;
+        led_state = !led_state;
+        digitalWrite(LED_BUILTIN, led_state);
     }
 }
